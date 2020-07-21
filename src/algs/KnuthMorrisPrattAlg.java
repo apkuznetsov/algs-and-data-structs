d3697003022d84e7f538694b37e826942d34ff78
Andrey <kouznts@ya.ru>
2020-07-21 11:56:20 +0400

    impl KMP alg

--- src/algs/KnuthMorrisPrattAlg.java
+++ src/algs/KnuthMorrisPrattAlg.java
@@ -1,8 +1,39 @@
 package algs;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class KnuthMorrisPrattAlg {
 
-    public static int[] calcPrefixFunc(String str) {
+    public static @NotNull
+    List<Integer> searchOccurrencesByKmpAlg(@NotNull String text, @NotNull String pattern) {
+
+        int[] prefixFunc = calcPrefixFunc(pattern);
+        ArrayList<Integer> occurrences = new ArrayList<Integer>();
+
+        int patternIndex = 0;
+        for (int textIndex = 0; textIndex < text.length(); textIndex++) {
+
+            while (patternIndex > 0 && text.charAt(textIndex) != pattern.charAt(patternIndex)) {
+                patternIndex = prefixFunc[patternIndex - 1];
+            }
+
+            if (text.charAt(textIndex) == pattern.charAt(patternIndex)) {
+                patternIndex += 1;
+            }
+
+            if (patternIndex == pattern.length()) {
+                occurrences.add(textIndex - patternIndex + 1);
+                patternIndex = prefixFunc[patternIndex - 1];
+            }
+        }
+
+        return occurrences;
+    }
+
+    public static int[] calcPrefixFunc(@NotNull String str) {
 
         int[] prefixFunc = new int[str.length()];
 
@@ -23,4 +54,5 @@ public class KnuthMorrisPrattAlg {
 
         return prefixFunc;
     }
+
 }
